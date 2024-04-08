556dd9a6cbc8daba9dad89f563e76117808769f6
Jet <taijunjet@hotmail.com>
2024-04-08 13:55:37 +0100

    don't need manual install

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
-echo "Remember to run 'M-m I' to install plugins once inside tmux"
+~/.config/tmux/plugins/tpm/bin/install_plugins
