afc0cc56108e2580a5d07885d958aaf082939248
Jet <taijunjet@hotmail.com>
2024-06-04 00:01:11 +0900

    grammar

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 # check if tmux exists
 if ! command -v tmux &> /dev/null
 then
-    echo "tmux not found, installing from apt"
-    sudo apt install tmux
+  echo "tmux not found, installing from apt..."
+  sudo apt update
+  sudo apt install tmux
 fi
 
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
